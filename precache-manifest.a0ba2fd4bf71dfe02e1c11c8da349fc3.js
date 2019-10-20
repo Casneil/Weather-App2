ffeb32a3f86641cdf56967ff246473ea508d0f06
Casneil Simpson <casneil.simpson@gmail.com>
2019-10-20 05:15:15 +0200

    Updates

---
+++ precache-manifest.a0ba2fd4bf71dfe02e1c11c8da349fc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3199c60036011cbda266c506539e4a21",
+    "url": "/Weather-App2/index.html"
+  },
+  {
+    "revision": "92e91692080a067955a0",
+    "url": "/Weather-App2/static/css/main.74acb8d2.chunk.css"
+  },
+  {
+    "revision": "3955e7de6322e7bef8cf",
+    "url": "/Weather-App2/static/js/2.69daf5d0.chunk.js"
+  },
+  {
+    "revision": "92e91692080a067955a0",
+    "url": "/Weather-App2/static/js/main.7b039089.chunk.js"
+  },
+  {
+    "revision": "f48a90adca3b70ab4ed4",
+    "url": "/Weather-App2/static/js/runtime-main.2f6fdb6c.js"
+  },
+  {
+    "revision": "fcd414a6431e6f92dff6112e605c14ae",
+    "url": "/Weather-App2/static/media/sunshine.fcd414a6.jpg"
+  }
+]);
